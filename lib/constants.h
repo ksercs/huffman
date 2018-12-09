411f42656aa02ac88a0a5725068493de16ee85fc
ksercs <ksercs0@gmail.com>
2018-12-09 05:50:44 +0300

    fix bags

--- lib/constants.h
+++ lib/constants.h
@@ -7,10 +7,6 @@
 #include <utility>
 #include <map>
 
-#define VEC std::vector
-#define SS second
-#define FF first
-
 const uint32_t BUF_SIZE = 3;
 const uint32_t BLOCK_SIZE = 4 * (1u << 10);
 const uint64_t ALPHA = (1u << 8);
